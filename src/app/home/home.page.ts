20e62ed9cedfd06b1ef5634f7eb1bb4156b22ff1
Lucas <lucas@MacBook-Air-de-Lucas.local>
2019-10-05 15:56:21 +0200

    more feats with a bug

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, NgZone, ViewChild } from '@angular/core';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+
+declare var google: any;
 
 @Component({
   selector: 'app-home',
@@ -6,7 +9,49 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+ @ViewChild('Map', { static: false }) mapElement: ElementRef;
 
-  constructor() {}
+  map: any;
+    mapOptions: any;
+    location = {lat: null, lng: null};
+    markerOptions: any = {position: null, map: null, title: null};
+    marker: any;
+    apiKey: any = 'AIzaSyBOLDgXn-_WscB9xUaIDFiaPq2cDIZYl6U';
+
+  constructor(public geolocation: Geolocation) {
+
+     const script = document.createElement('script');
+     script.id = 'googleMap';
+     if (this.apiKey) {
+         script.src = 'https://maps.googleapis.com/maps/api/js?key=' + this.apiKey;
+     } else {
+         script.src = 'https://maps.googleapis.com/maps/api/js?key=';
+     }
+     document.head.appendChild(script)
+
+    this.geolocation.watchPosition().subscribe((data) => {
+      console.log("lat" + data.coords.latitude + "- long" + data.coords.longitude)
+      this.location.lat =  data.coords.latitude;
+      this.location.lng = data.coords.longitude;
+    });
+
+     this.mapOptions = {
+      center: this.location,
+      zoom: 21,
+      mapTypeControl: false
+    };
+
+    setTimeout(() => {
+      this.map = new google.maps.Map(this.mapElement.nativeElement, this.mapOptions);
+      
+      /*Opciones del marcador*/
+      this.markerOptions.position = this.location;
+      this.markerOptions.map = this.map;
+      this.markerOptions.title = 'My Location';
+      this.marker = new google.maps.Marker(this.markerOptions);
+    }, 3000);
+  }
+  
+  
 
 }
